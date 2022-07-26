d0971b4665dc7dddc477b45fec0edf950a0ded69
davydovMikhail <davydov27977@gmail.com>
2022-07-26 12:33:19 +0700

    join to campaign

--- contracts/IDO.sol
+++ contracts/IDO.sol
@@ -3,7 +3,7 @@ pragma solidity =0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import "./interfaces/IUniswapRouter.sol";
 
 contract Crepe {
     using SafeERC20 for IERC20Metadata;
@@ -20,23 +20,68 @@ contract Crepe {
     uint256 TokenRate;
     uint256 PaidOut;
 
+    IUniswapRouter router;
+    address USDC;
+    address crepe;
+
 
     mapping(address => bool) public AcceptedTokenList;
 
     mapping(address => UserInfo) public users;
 
-    function joinToCampaign(address _acceptedToken, uint256 _amount) external {
+    function getRate(uint256 _USDCBalance) internal view returns (uint256 rate) {
+        uint256 crepeBalance = IERC20Metadata(crepe).balanceOf(address(this));
+        rate = _USDCBalance / crepeBalance;
+    }
+
+    function joinToCampaign(address _acceptedToken, uint256 _amount) external payable {
         require(
             block.timestamp > StartIn,
             "Campaign start time has not come yet."
         );
+
         require(block.timestamp < EndIn, "Campaign time has expired.");
-
-        if(_acceptedToken == 0 && msg.value > 0) {
-
-        } else if(AcceptedTokenList[_acceptedToken]) {
-
+        uint256 balanceBefore = IERC20Metadata(USDC).balanceOf(address(this));
+        uint256 rate = getRate(balanceBefore);
+        if(_acceptedToken == address(0) && msg.value > 0) { // matic
+            address[] memory path = new address[](2);
+            path[0] = router.WETH();
+            path[1] = USDC;
+            router.swapExactETHForTokens{ value: msg.value }(
+                0,
+                path,
+                address(this),
+                block.timestamp + 30
+            );
+        } else if(AcceptedTokenList[_acceptedToken]) { // wbtc, weth
+            IERC20Metadata(_acceptedToken).safeTransferFrom(
+                msg.sender,
+                address(this),
+                _amount
+            );
+            address[] memory path = new address[](2);
+            path[0] = _acceptedToken;
+            path[1] = USDC;
+            router.swapExactTokensForTokens(
+                _amount,
+                0,
+                path,
+                address(this),
+                block.timestamp + 30
+            );
+        } else if(_acceptedToken == USDC) {
+            IERC20Metadata(_acceptedToken).safeTransferFrom(
+                msg.sender,
+                address(this),
+                _amount
+            );
         }
+        uint256 amountUSDC = IERC20Metadata(USDC).balanceOf(address(this)) - balanceBefore;
+        uint256 amountToReceive = amountUSDC / rate;
+        users[msg.sender] = UserInfo({
+            Accumulated: amountToReceive, 
+            Received: false
+        });
     }
 
     // function claimToken() external {
---
+++ contracts/interfaces/IUniswapRouter.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2;
+
+interface IUniswapRouter {
+
+    function WETH() external pure returns (address);
+
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+}
