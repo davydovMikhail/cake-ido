7cbfa0c51e7cbe732517a3a6a986254e54e4447b
davydovMikhail <davydov27977@gmail.com>
2022-07-27 08:50:33 +0700

    adder for accepteds addreses

--- contracts/IDO.sol
+++ contracts/IDO.sol
@@ -23,12 +23,17 @@ contract Crepe {
     address USDC;
     address crepe;
 
-
     mapping(address => bool) public AcceptedTokenList;
 
     mapping(address => UserInfo) public Users;
 
-    constructor(uint256 _startTime, uint256 _endTime, uint256 _unlockClaimTime, address _crepe, address _stableToken) {
+    constructor(
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _unlockClaimTime,
+        address _crepe,
+        address _stableToken
+    ) {
         StartIn = _startTime;
         EndIn = _endTime;
         UnlockClaimTime = _unlockClaimTime;
@@ -36,25 +41,29 @@ contract Crepe {
         USDC = _stableToken;
     }
 
-
-    function joinToCampaign(address _acceptedToken, uint256 _amount) external payable {
+    function joinToCampaign(address _acceptedToken, uint256 _amount)
+        external
+        payable
+    {
         require(
             block.timestamp > StartIn,
             "Campaign start time has not come yet."
         );
         require(block.timestamp < EndIn, "Campaign time has expired.");
         uint256 balanceBefore = IERC20Metadata(USDC).balanceOf(address(this));
-        if(_acceptedToken == address(0) && msg.value > 0) { // matic
+        if (_acceptedToken == address(0) && msg.value > 0) {
+            // matic
             address[] memory path = new address[](2);
             path[0] = router.WETH();
             path[1] = USDC;
-            router.swapExactETHForTokens{ value: msg.value }(
+            router.swapExactETHForTokens{value: msg.value}(
                 0,
                 path,
                 address(this),
                 block.timestamp + 30
             );
-        } else if(AcceptedTokenList[_acceptedToken]) { // wbtc, weth
+        } else if (AcceptedTokenList[_acceptedToken]) {
+            // wbtc, weth
             IERC20Metadata(_acceptedToken).safeTransferFrom(
                 msg.sender,
                 address(this),
@@ -70,29 +79,35 @@ contract Crepe {
                 address(this),
                 block.timestamp + 30
             );
-        } else if(_acceptedToken == USDC) {
+        } else if (_acceptedToken == USDC) {
             IERC20Metadata(_acceptedToken).safeTransferFrom(
                 msg.sender,
                 address(this),
                 _amount
             );
         }
-        uint256 amountUSDC = IERC20Metadata(USDC).balanceOf(address(this)) - balanceBefore;
+        uint256 amountUSDC = IERC20Metadata(USDC).balanceOf(address(this)) -
+            balanceBefore;
         Users[msg.sender] = UserInfo({
-            Accumulated: amountUSDC, 
+            Accumulated: amountUSDC,
             Received: false
         });
         TotalAccumulated += amountUSDC;
     }
 
     function claimToken() external {
-        require(block.timestamp > UnlockClaimTime, "The time of the unlock has not yet come");
-        require(!Users[msg.sender].Received, "Tokens claimed already");
-        uint256 share = ( Users[msg.sender].Accumulated * TotalCrepe ) / TotalAccumulated;
-        IERC20Metadata(crepe).safeTransfer(
-            msg.sender,
-            share
+        require(
+            block.timestamp > UnlockClaimTime,
+            "The time of the unlock has not yet come"
         );
+        require(!Users[msg.sender].Received, "Tokens claimed already");
+        uint256 share = (Users[msg.sender].Accumulated * TotalCrepe) /
+            TotalAccumulated;
+        IERC20Metadata(crepe).safeTransfer(msg.sender, share);
         Users[msg.sender].Received = true;
+    }
+
+    function addAcceptedToken(address _newToken) external {
+        AcceptedTokenList[_newToken] = !AcceptedTokenList[_newToken];
     }
 }
